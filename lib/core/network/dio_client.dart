4d082a0cf98d32fda404ca7e742dad51f82bad10
Hippro2004 <chaiprach.2530@gmail.com>
2026-07-15 14:35:15 +0700

    Update dio_client.dart

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class DioClient {
   static final Dio dio = Dio(
     BaseOptions(
-      baseUrl: "http://192.168.1.60:8081/api/v1",
+      baseUrl: "http://172.20.10.3:8081/api/v1",
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
       headers: {"Content-Type": "application/json"},
