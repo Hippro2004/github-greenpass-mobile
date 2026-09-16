ce394e95a7ea3cd95fb92e3dde3d59989c689dfc
Hippro2004 <chaiprach.2530@gmail.com>
2026-09-16 12:38:23 +0700

    Update dio_client.dart

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 class DioClient {
-  static const String _defaultBaseUrl = "http://172.20.10.3:8081/api/v1";
+  static const String _defaultBaseUrl = "http://[IP_ADDRESS]/api/v1";
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
     defaultValue: _defaultBaseUrl,
